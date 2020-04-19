ea5843a06f2597878290579bec621aabf1d0b772
Chris Ingoe <chris.software.engineer@gmail.com>
2020-04-19 22:59:43 +0100

    add meta data to the _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,14 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 export default class DocTemplate extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en-GB">
         <Head>
-          <title>Template project</title>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta name="Description" content="NextJS web application" />
+          <link rel="icon" href="static/img/favicon.ico" />
         </Head>
         <body>
           <Main />
